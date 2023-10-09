0d39bb4b6577d6e0ad98c47ff9903d86c734f145
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-10-09 16:55:47 -0700

    add in function

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var submitBtn = document.querySelector("#submit-btn");
 
 function getWeather () {
     console.log("CITY VALUE: ", cityValue.value)
-    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?" + cityValue.value + "&appid=" + apiKey;
+    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityValue.value + "&appid=" + apiKey;
     fetch(queryUrl)
     .then(function(response) {
         console.log("Response: ", response);
@@ -12,15 +12,37 @@ function getWeather () {
     })
     .then(function(data) {
         console.log("DATA: ", data)
+        filterForecast(data.list)
     })
 }
-//var info = [(variables)
-//var date = [#date]
-//var temp = [#temp]
-//var icon = [#icon]
-//varwind = [#wind]
-//var humidity = [#humidity]  
-//] 
+// for(x = 0; x >= 0; x++){
+//     console.log ('temp'+ [x])
+// }
+// for(x = 0; x >= 0; x++){
+//     console.log ('temp'+ [x])
+// }
+// for(x = 0; x >= 0; x++){
+//     console.log ('temp'+ [x])
+// }
+// for(x = 0; x >= 0; x++){
+//     console.log ('temp'+ [x])
+// }
+// for(x = 0; x >= 0; x++){
+//     console.log ('temp'+ [x])
+// }
+
+function filterForecast(allWeatherForecasts) {
+    console.log(allWeatherForecasts)
+    console.log()
+    var arr = {
+        date: data.date,
+        temp: data.temp,
+        wind: data.wind,
+        humidity: data.humidity
+    }
+    console.log(date+'',temp+'',wind+'',+humidity);
+
+}
 
 
 submitBtn.addEventListener("click", getWeather);  
