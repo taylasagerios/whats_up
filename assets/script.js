70e7a69ddb9e76bf891ccea4be406b52a27d50e7
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-10-10 19:39:09 -0700

    added in cards and corrected which content

--- assets/script.js
+++ assets/script.js
@@ -2,35 +2,21 @@ var apiKey = "362cbe9d61e170732a21343e28d7a203";
 var cityValue = document.querySelector("#input-element");
 var submitBtn = document.querySelector("#submit-btn");
 
-function getWeather () {
+function getWeather() {
     console.log("CITY VALUE: ", cityValue.value)
-    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityValue.value + "&appid=" + apiKey;
+    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityValue.value + "&appid=" + apiKey + "&units=imperial";
     fetch(queryUrl)
-    .then(function(response) {
-        console.log("Response: ", response);
-        return response.json();
-    })
-    .then(function(data) {
-        console.log("DATA: ", data)
-        var forecast = filterForecast(data.list)
-        fillForecastHtml(forecast)
-    })
+        .then(function (response) {
+            console.log("Response: ", response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log("DATA: ", data)
+            var forecast = filterForecast(data.list)
+            fillForecastHtml(forecast)
+        })
 }
-// for(x = 0; x >= 0; x++){
-//     console.log ('temp'+ [x])
-// }
-// for(x = 0; x >= 0; x++){
-//     console.log ('temp'+ [x])
-// }
-// for(x = 0; x >= 0; x++){
-//     console.log ('temp'+ [x])
-// }
-// for(x = 0; x >= 0; x++){
-//     console.log ('temp'+ [x])
-// }
-// for(x = 0; x >= 0; x++){
-//     console.log ('temp'+ [x])
-// }
+
 
 function filterForecast(allWeatherForecasts) {
     console.log(allWeatherForecasts)
@@ -41,24 +27,40 @@ function filterForecast(allWeatherForecasts) {
     //     wind: data.wind,
     //     humidity: data.main.humidity
     // }
-   var forecast = []
-    for (var i =0;i< allWeatherForecasts.length; i+=8) {
+    var fivedayforecast = document.querySelector("#five-day-forecast")
+    var forecast = []
+    for (var i = 0; i < allWeatherForecasts.length; i += 8) {
         var data = allWeatherForecasts[i]
-        forecast.push({
-            date: data.dt,
+        var forecastData = {
+            date: data.dt_txt,
             temp: data.main.temp,
-            icon: data.icon,
+            icon: data.weather[0].icon,
             wind: data.wind.speed,
             humidity: data.main.humidity
-        })
+        }
+        var card = document.createElement("div")
+        card.setAttribute("class", "col-2 card card-rounded border-color-black bg-grey" )
+        var temp= document.createElement("p")
+        var wind= document.createElement("p")
+        var humidity= document.createElement("p")
+        var date= document.createElement("p")
+        var img= document.createElement("img")
+        img.setAttribute("src", `https://openweathermap.org/img/wn/${forecastData.icon}@2x.png`)
+        temp.textContent= "Temp:" + forecastData.temp
+        wind.textContent= forecastData.wind
+        humidity.textContent= forecastData.humidity
+        date.textContent= forecastData.date
+        card.append(date, temp, wind, humidity, img)
+        fivedayforecast.append(card)
+        
     }
     // console.log(date+'',temp+'',wind+'',+humidity);
-    console.log("forecast:",forecast)
-return forecast
+    console.log("forecast:", forecast)
+    return forecast
 }
 
 function fillForecastHtml(forecast) {
-    for(var x=0; x< forecast.length; x++){
+    for (var x = 0; x < forecast.length; x++) {
         var tempEl = document.querySelector(`#temp-${x}`)
         console.log(tempEl)
 
